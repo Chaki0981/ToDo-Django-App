84869944892e75effbdae4a424546bc933015dd7
Kacper Wozniak <kacperwozniak@gmail.com>
2023-04-20 01:16:24 +0200

    Added All and archived tasks view and function buttons; Basic style added;

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.TasksView.as_view(), name='starting-page'),
+    path('all-tasks', views.AllTasksView.as_view(), name='all-tasks-page'),
+    path('archive-tasks', views.ArchiveTasksView.as_view(), name='archived-tasks-page'),
     path('<int:id>', views.TaskDetailView.as_view(), name='task-detail-page'),
     path('complete-task/<int:id>', views.CompleteTaskView.as_view(), name='complete-task-page'),
     path('remove-task/<int:id>', views.RemoveTaskView.as_view(), name='remove-task-page'),
+    path('move-to-archive/<int:id>', views.MoveToArchiveView.as_view(), name='move-to-archive-page'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,34 @@ from django.views import View
 from .models import Task
 from .forms import AddTaskForm, AddTaskFormAdvance
 
-class TasksView(View):
+class GeneralTasksView():
+    def post(self, request):
+        form = AddTaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        context = {
+            'form': form,
+        }
+
+        return redirect('starting-page')
+
+class TasksView(View, GeneralTasksView):
+    def get(self, request):
+        tasks_temp = Task.objects.filter(is_done=False)
+        tasks = tasks_temp.filter(is_archived=False)
+        form = AddTaskForm()
+
+        context = {
+            'tasks': tasks,
+            'form': form,
+        }
+
+        return render(request, 'todo/index.html', context)
+    
+    
+class AllTasksView(View, GeneralTasksView):
     def get(self, request):
         tasks = Task.objects.all()
         form = AddTaskForm()
@@ -14,21 +41,18 @@ class TasksView(View):
         }
 
         return render(request, 'todo/index.html', context)
-    
-    def post(self, request):
-        tasks = Task.objects.all()
-        form = AddTaskForm(request.POST)
 
-        if form.is_valid():
-            form.save()
+
+class ArchiveTasksView(View):
+    def get(self, request):
+        tasks = Task.objects.filter(is_archived=True)
 
         context = {
             'tasks': tasks,
-            'form': form,
         }
 
-        return redirect('starting-page')
-    
+        return render(request, 'todo/index.html', context)
+
 class TaskDetailView(View):
     def get(self, request, id):
         task = Task.objects.get(pk=id)
@@ -60,6 +84,14 @@ class CompleteTaskView(View):
     def get(self, request, id):
         task = Task.objects.get(pk=id)
         task.is_done = True
+        task.save()
+
+        return redirect('starting-page')
+    
+class MoveToArchiveView(View):
+    def get(self, request, id):
+        task = Task.objects.get(pk=id)
+        task.is_archived = True
         task.save()
 
         return redirect('starting-page')
